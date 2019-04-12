0cf4d9b74defe68497a7f73db8cba2978906d0a4
Senthil <senthil@vivantatechnologies.com>
2019-04-12 15:13:34 +0530

    Updates

---
+++ precache-manifest.7d74ca62388b8f0b28eb26da84640ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-buildpack/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ea3fdad547723c858d97",
+    "url": "/react-buildpack/static/js/runtime~main.15ef6aab.js"
+  },
+  {
+    "revision": "74b2a3a1fdaaabe7a4af",
+    "url": "/react-buildpack/static/js/main.d90e5afb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-buildpack/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "74b2a3a1fdaaabe7a4af",
+    "url": "/react-buildpack/static/css/main.43fd19e4.chunk.css"
+  },
+  {
+    "revision": "f9eba3b4c6ea81a7b4f71d23518da0f9",
+    "url": "/react-buildpack/index.html"
+  }
+];
